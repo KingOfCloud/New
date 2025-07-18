2da6bc7aa4f07b423d632bc94f79599a968881eb
KingOfCloud <meganathanindia1998@gmail.com>
2025-07-18 11:54:47 +0530

    commit

--- src/org/newone/NEW.java
+++ src/org/newone/NEW.java
@@ -5,7 +5,9 @@ public class NEW {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println("Meganathan");
 		System.out.println("Chennai");
+		
 	}
 
 }
